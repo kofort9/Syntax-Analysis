dc799f6e22352898d3895fc16ebff1777de68eb6
Kofort9 <kofort9@gmail.com>
2020-03-25 20:30:26 -0400

    Added Function declarations, and  character class, token codes  they will be used to parse the file.

--- front.c
+++ front.c
@@ -14,3 +14,25 @@ int lexLen;
 int token;
 int nextToken;
 FILE *in_fp, *fopen();
+
+/*Function declarations */
+
+void addChar();
+void getChar();
+void getNonBlank();
+int lex();
+
+/* Character classes */
+#define LETTER 0
+#define DIGIT 1
+#define UNKNOWN 99
+
+/* Token codes */
+#define INT_LIT 10
+#define ASSIGN_OP 20
+#define ADD_OP 21
+#define SUB_OP 22
+#define MULT_OP 23
+#define DIV_OP 24
+#define LEFT_PAREN 25
+#define RIGHT_PAREN 26
