eb2f4f1626a6345cce43451bee8c1870db6e2d73
Kofort9 <kofort9@gmail.com>
2020-03-26 14:52:53 -0400

    Added addChar() and getchar()
    addChar - a function to add nextChar to lexeme
    
    getChar - a fucntion to ge the next character of input and determine its character class

--- front.c
+++ front.c
@@ -94,6 +94,30 @@ int lookup (char ch) {
     return nextToken;
   }
 
+/************************************************/
+/* addChar - a function to add nextChar to lexeme */
+void addChar() {
+  if (lexLen <= 98) {
+    lexeme[lexLen++] = nextChar;
+    lexeme[lexLen] = 0;
+  } else {
+    printf("Error - lexeme is to long \n");
+  }
+}
 
+/************************************************/
+/* getChar - a function to get the next character of
+              input and determine its character class */
+void getChar() {
+  if ((nextChar = getc(in_fp)) != EOF) {
+    if (isalpha(nextChar)) {
+      charClass = LETTER;
+    } else if (isdigit (nextChar)) {
+      charClass = DIGIT;
+    } else {
+      charClass = UNKNOWN;
+    }
+  } else {
+      charClass = EOF;
   }
 }
